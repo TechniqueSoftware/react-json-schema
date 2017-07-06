f176e12851c1859a5a188d349f824fa3fec50b68
Nick Laffey <nick.laffey@gmail.com>
2017-07-06 15:15:10 -0400

    Updating dist files

--- dist/react-json-schema.js
+++ dist/react-json-schema.js
@@ -4,15 +4,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var _componentMap = new WeakMap();
+var componentMapCollection = new WeakMap();
 
 var ReactJsonSchema = function () {
   function ReactJsonSchema() {
@@ -24,7 +24,7 @@ var ReactJsonSchema = function () {
     value: function parseSchema(schema) {
       var element = null;
       var elements = null;
-      if (Array.isArray(schema)) {
+      if (Object.prototype.toString.call(schema) === '[object Array]') {
         elements = this.parseSubSchemas(schema);
       } else {
         element = this.createComponent(schema);
@@ -34,50 +34,31 @@ var ReactJsonSchema = function () {
   }, {
     key: 'parseSubSchemas',
     value: function parseSubSchemas() {
+      var _this = this;
+
       var subSchemas = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
 
       var Components = [];
       var index = 0;
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
-
-      try {
-        for (var _iterator = subSchemas[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var subSchema = _step.value;
-
-          subSchema.key = typeof subSchema.key !== 'undefined' ? subSchema.key : index;
-          Components.push(this.parseSchema(subSchema));
-          index++;
-        }
-      } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
-          }
-        } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
-          }
-        }
-      }
-
+      Object.keys(subSchemas).forEach(function (key) {
+        var subSchema = subSchemas[key];
+        subSchema.key = typeof subSchema.key !== 'undefined' ? subSchema.key : index;
+        Components.push(_this.parseSchema(subSchema));
+        index++;
+      });
       return Components;
     }
   }, {
     key: 'createComponent',
     value: function createComponent(schema) {
-      var component = schema.component,
-          children = schema.children,
-          text = schema.text,
-          rest = _objectWithoutProperties(schema, ['component', 'children', 'text']);
+      var props = _extends({}, schema);
+      delete props.component;
+      delete props.children;
+      delete props.text;
 
       var Component = this.resolveComponent(schema);
-      var Children = typeof text !== 'undefined' ? text : this.resolveComponentChildren(schema);
-      return (0, _react.createElement)(Component, rest, Children);
+      var Children = typeof schema.text !== 'undefined' ? schema.text : this.resolveComponentChildren(schema);
+      return (0, _react.createElement)(Component, props, Children);
     }
   }, {
     key: 'resolveComponent',
@@ -93,7 +74,7 @@ var ReactJsonSchema = function () {
           Component = schema.component;
         }
       } else {
-        throw new Error('ReactJsonSchema could not resolve a component due to a missing component attribute in the schema.');
+        throw new Error('ReactJsonSchema could not resolve a component due to a missing component \n          attribute in the schema.');
       }
       return Component;
     }
@@ -105,12 +86,12 @@ var ReactJsonSchema = function () {
   }, {
     key: 'getComponentMap',
     value: function getComponentMap() {
-      return _componentMap.get(this);
+      return componentMapCollection.get(this);
     }
   }, {
     key: 'setComponentMap',
     value: function setComponentMap(componentMap) {
-      _componentMap.set(this, componentMap);
+      componentMapCollection.set(this, componentMap);
     }
   }]);
 
