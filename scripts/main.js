31c139ec658dce5e08902d5c637c8ad0a177c022
elliottisonfire <matt.projectmedium@gmail.com>
2018-08-10 13:49:07 -0400

    Site with examples

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
 (function () {
-  var $CONTENT = document.getElementById('content');
-  var playground = new ReactJsonSchema.default();
-  playground.setComponentMap({
-    Swiper: ReactIdSwiper
-  });
-
-  var editor = ace.edit('editor', {
+  var aceConfig = {
     mode: 'ace/mode/json',
     highlightActiveLine: false,
     showFoldWidgets: false,
@@ -17,20 +11,48 @@
     useSoftTabs: false,
     tabSize: 2,
     showGutter: false,
+  };
+
+  var exampleConfig = [{
+    id: 'formExample',
+    outputId: 'formExampleOutput',
+  }, {
+    id: 'swiperExample',
+    outputId: 'swiperExampleOutput',
+    rjsComponentMap: {
+      Swiper: ReactIdSwiper
+    }
+  }];
+
+  var examples = [];
+  exampleConfig.forEach(function (config) {
+    var rjsInstance = new ReactJsonSchema.default();
+    if (config.rjsComponentMap) {
+      rjsInstance.setComponentMap(config.rjsComponentMap);
+    }
+    var editor = ace.edit(config.id, aceConfig);
+    var example = {
+      id: config.id,
+      outputId: config.outputId,
+      rjs: rjsInstance,
+      editor: editor
+    };
+    editor.on('change', function (event) {
+      renderData(example, event);
+    });
+    examples.push(example);
+    renderData(example);
   });
 
-  function renderData() {
+  function renderData(example, event) {
+    var $content = document.getElementById(example.outputId);
     try {
-      var data = JSON.parse(editor.getValue());
-      ReactDOM.render(playground.parseSchema(data), $CONTENT);
+      var data = JSON.parse(example.editor.getValue());
+      ReactDOM.render(example.rjs.parseSchema(data), $content);
     } catch (e) {
       ReactDOM.render(React.createElement('span', {
         'style': { 'color': 'rgb(238, 16, 0)' }
-      }, e.message), $CONTENT);
+      }, e.message), $content);
     }
   }
-
-  editor.on('change', renderData);
-
-  renderData();
 })();
